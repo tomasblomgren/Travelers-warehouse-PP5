d8f7a9decac569417de2f1b02c7788338889205a
tomasblomgren <Tomas.blomgren96@gmail.com>
2023-03-08 14:57:14 +0000

    adding search queries

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -27,3 +28,8 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Favourites(models.ManyToManyField):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    favourites = models.ManyToManyField(User, related_name='favourites', default=None, blank=True)
--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -8,9 +10,21 @@ def all_products(request):
     """ A view to show all products including sorting and search queries """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
@@ -26,3 +40,15 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def favourites(request, product_id):
+    """ A view to show the favourites to the user """
+
+    favourites = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'favourites': favourites,
+    }
+
+    return render(request, 'templates/includes/favourites.html', context)
