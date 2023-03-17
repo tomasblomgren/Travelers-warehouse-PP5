bb55e86144f51f16f6e872b285e61d0ce4382d75
tomasblomgren <Tomas.blomgren96@gmail.com>
2023-03-17 14:37:20 +0000

    added bag app in project repo

--- products/views.py
+++ products/views.py
@@ -12,7 +12,7 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
-    category = None
+    categories = None
 
     if request.GET:
         if 'category' in request.GET:
