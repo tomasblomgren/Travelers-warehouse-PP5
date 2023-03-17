128fb8fe9b7f7c27e1bbca24ea3566f2fff59c52
tomasblomgren <Tomas.blomgren96@gmail.com>
2023-03-17 16:10:44 +0000

    adding href to favourites anchor tag

--- products/views.py
+++ products/views.py
@@ -4,8 +4,6 @@ from django.db.models import Q, Sum
 from django.contrib.auth.decorators import login_required
 from .models import Product, Category, Favourite, Order
 
-# Create your views here.
-
 
 def all_products(request):
     """ A view to show all products including sorting and search queries """
