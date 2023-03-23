69887925dfa0c71eb3f1d0a842712ece65236cb9
tomasblomgren <Tomas.blomgren96@gmail.com>
2023-03-23 18:46:11 +0000

    adding js functionality to send favourites to correct page

--- checkout/models.py
+++ checkout/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.core.validators import EmailValidator, RegexValidator, MinLengthValidator, MaxLengthValidator
 from django.db.models import Sum
 from django.conf import settings
+from django.contrib.auth.models import User
 
 from products.models import Product
 
@@ -67,3 +68,8 @@ class OrderLineItem(models.Model):
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False, editable=False)
 
+
+class Bookmark(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.URLField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
