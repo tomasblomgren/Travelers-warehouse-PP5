31e34160db2c34f69573c0d8a856a12018c2d64a
tomasblomgren <Tomas.blomgren96@gmail.com>
2023-04-05 13:08:38 +0000

    deployment settings, localhost

--- bag/models.py
+++ bag/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class emailform(models.Model):
+    
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=50, null=False, blank=False)
+    country = models.CharField(max_length=50, null=False, blank=False)
--- travelers_warehouse/settings.py
+++ travelers_warehouse/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-=f^o_mqzgce^nd35rjyai#*6hf+d8554gjq7lca0k2*mq_a6!!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['travelers-warehouse.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -113,13 +114,17 @@ WSGI_APPLICATION = 'travelers_warehouse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
