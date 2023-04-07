1b94e5d2ca1fabfcb39d2a290e9e6fac32160e65
tomasblomgren <Tomas.blomgren96@gmail.com>
2023-04-07 15:42:38 +0000

    finishing up orderform now setting up views

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,11 @@ def view_bag(request):
     return render(request, 'checkout/templates/checkout.html')
 
 
+def view_orderform(request):
+""" A view to render the bag contents """
+return render(request, 'checkout/templates/checkout.html')
+
+
 def favourites(request):
     """ A view to render the bag contents """
 
