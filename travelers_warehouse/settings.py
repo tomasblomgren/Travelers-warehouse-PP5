8681baab13faccc8ff930acc3418fcbb4dd8514f
tomasblomgren <Tomas.blomgren96@gmail.com>
2023-04-11 13:50:38 +0000

    changing debug to false

--- travelers_warehouse/settings.py
+++ travelers_warehouse/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=f^o_mqzgce^nd35rjyai#*6hf+d8554gjq7lca0k2*mq_a6!!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['travelers-warehouse.herokuapp.com', 'localhost']
 
