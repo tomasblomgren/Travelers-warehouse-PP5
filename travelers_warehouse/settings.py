ea0af0515bd619e3699f89eb3cc3a5a405290685
tomasblomgren <Tomas.blomgren96@gmail.com>
2023-04-11 13:58:11 +0000

    adding static root

--- travelers_warehouse/settings.py
+++ travelers_warehouse/settings.py
@@ -178,7 +178,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
